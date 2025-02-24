56c1bbc5dd284a4e7a142a01d8038e888b7f1618
Natan Mateus <69116867+NatanMateuscs@users.noreply.github.com>
2025-02-23 23:25:55 -0300

    test

---
+++ Ideias/map1.js
@@ -0,0 +1,18 @@
+const carrinho = [
+    '{"nome": "lapiseira", "preco": 2,50}',
+    '{"nome": "lapis", "preco": 1,50}',
+    '{"nome": "caneta", "preco": 1,99}',
+    '{"nome": "borracha", "preco": 1,00}',
+    '{"nome": "caderno", "preco": 25,90}',
+    '{"nome": "mochila", "preco": 79,99}',
+]
+
+//Retornar um array apenas com os preços 
+
+const paraObjeto = json => JSON.parse(json)
+const apenasPreço = produto => produto.preço
+
+const resultado = carrinho.map(paraObjeto)
+
+console.log(json)
+
