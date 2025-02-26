bda25a3adbf205f1191664e5450404578c96d84e
Natan Mateus <69116867+NatanMateuscs@users.noreply.github.com>
2025-02-26 14:32:11 -0300

    MAis estudo

---
+++ aula 15/index.js
@@ -0,0 +1,16 @@
+let num1 = 25;
+
+//console.log(100 / 0 ) //numero infinity...
+//console.log(num1 ** 0.5) //pegando raiz quadrada de um numero
+//console.log(num1 ** (1/2)) //pegando raiz quadrada de um numero
+//let num2 = Math.floor(num1) //Arredondar
+//let num2 = Math.ceil(num1) //Arredondar para cima 
+//let num2 = Math.round(num1) //Arredondar para cima 
+
+//console.log(Math.max(1, 2, 3, 4, 5, -10, -50, -100)); //pegar o MAIOR numero desta sequencia
+//console.log(Math.min(1, 2, 3, 4, 5, -10, -50, -100)); //pegar o MENOR numero desta sequencia
+//const aleatorio = Math.round(Math.random() * (10 - 5) + 1) ; // gera numero aleatorio e arrendonda ele
+
+
+//console.log(Math.pow(2, 10)); //elevando um numero usando math
+//console.log(2 ** 10); //elevando um sem usar math
